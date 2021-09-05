545c93c7c4c2e2cf9efc2c572ffa5c923c28afb5
Jcanotorr06 <jcano062001@gmail.com>
2021-09-05 15:50:46 -0500

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5cb25c0e637ea68b379ea944e4339dd1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a694492e-d02c-44c2-87f0-02fe792ed783'
+};
